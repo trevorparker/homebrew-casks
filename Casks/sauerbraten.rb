9d23d867cc68ae1271db85f6660975e9dc43f115
Trevor Parker <trevor@trevorparker.com>
2014-08-17 17:43:54 -0400

    Add Sauerbraten

---
+++ Casks/sauerbraten.rb
@@ -0,0 +1,9 @@
+class Sauerbraten < Cask
+  version '2013_01_26_collect_edition'
+  sha256 'c6807484fa0d2c42ac774b97f9b21e9eace720e4403abc11b0321b2645d3589d'
+
+	url "https://downloads.sourceforge.net/sourceforge/sauerbraten/sauerbraten_#{version}_macosx.dmg"
+	homepage 'http://sauerbraten.org/'
+
+	link 'Sauerbraten.app'
+end
